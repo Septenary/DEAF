73f0a2b26901f3c556388170c023c0937f62627d
Septenary <sevenfm2@gmail.com>
2017-09-12 17:51:23 -0400

    Update DEAF.lua

--- DEAF.lua
+++ DEAF.lua
@@ -1,6 +1,8 @@
 -- Created by Sevenfm on Baconetworks, for herself and anyone who can't figure out how the hell to automate infusion crafting
 
 -- Hook injectors
+local component = require("component")
+
 local counter = 1
 local injectors = {}
 for address, componentType in component.list("inventory_controller") do
